217225ff2d6ee2daed068624abf0249da95f08a8
Jessicatcheyanou <jessicatcheyanou@gmail.com>
2022-02-16 13:06:40 -0500

    Jess | Add and Save a new Recipe

--- src/main/java/com/example/rest/controller/RecipesController.java
+++ src/main/java/com/example/rest/controller/RecipesController.java
@@ -20,35 +20,37 @@ public class RecipesController {
     @Autowired
     private RecipesService recipesService;
 
+    //Get all recipes
     @GetMapping("/recipes")
     public ResponseEntity<Iterable<Recipes>> getAllRecipes() {
         Iterable<Recipes> recipesList = recipesService.getAllRecipes();
         return new ResponseEntity<>(recipesList, HttpStatus.OK);
     }
 
+    //Get the name of all the Recipes
     @GetMapping("/recipes/recipesNames")
     public ResponseEntity<List<String>> getAllRecipesNames() {
         List<String> recipesNamesList = recipesService.getAllRecipesNames();
         return new ResponseEntity<>(recipesNamesList,HttpStatus.OK);
     }
 
-    //Get a recipe`s List of Ingredients and Number of steps
+    //Get the following recipe`s details: List of Ingredients and Number of steps
     @GetMapping("/recipes/details/{recipeName}")
     public ResponseEntity<Details> getARecipeDetails(@PathVariable String recipeName) {
         return new ResponseEntity<>(recipesService.getRecipeDetails(recipeName),HttpStatus.OK);
     }
 
+    //Get all information concerning a Recipe
     @GetMapping({"/recipes/{recipesName}"})
     public ResponseEntity<Recipes> getRecipes(@PathVariable String recipesName){
         return new ResponseEntity<>(recipesService.getRecipesById(recipesName),HttpStatus.OK);
     }
 
-    @PostMapping("")
+    //Add and save a new Recipe
+    @PostMapping("/recipes")
     public ResponseEntity<Recipes> saveRecipes(@RequestBody Recipes recipes){
         Recipes recipes1 = recipesService.addRecipes(recipes);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("recipes","/data"+recipes.getName());
-        return new ResponseEntity<>(recipes1,httpHeaders,HttpStatus.CREATED);
+        return new ResponseEntity<>(recipes1,HttpStatus.CREATED);
     }
 
     @PutMapping({"/recipes/{recipesId}"})
